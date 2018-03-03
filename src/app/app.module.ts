feb106d827149e305d29b13f2b963c330e024a66
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-03-03 11:29:42 +0100

    WiP on reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
-import { environment } from '../environments/environment';
-import { AngularFirestore} from 'angularfire2/firestore';
-import { MessageFormComponent } from './message-form/message-form.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {environment} from '../environments/environment';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {MessageFormComponent} from './message-form/message-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-	imports: [
-		BrowserModule,
-		AngularFireModule.initializeApp(environment.firebase)
-	],
-	providers: [AngularFirestore],
-	declarations: [ AppComponent, MessageFormComponent ],
-	bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [AngularFirestore],
+    declarations: [AppComponent, MessageFormComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/message-form/message-form.component.ts
+++ src/app/message-form/message-form.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/filter';
+
 
 @Component({
-  selector: 'app-message-form',
-  templateUrl: './message-form.component.html',
-  styleUrls: ['./message-form.component.css']
+    selector: 'app-message-form',
+    templateUrl: './message-form.component.html',
+    styleUrls: ['./message-form.component.css']
 })
-export class MessageFormComponent implements OnInit {
+export class MessageFormComponent {
+    @Input() public submitButtonText = 'Submit';
+    public form: FormGroup;
 
-  constructor() { }
+    @Output() formSubmit = new EventEmitter();
+    formSubmitSubject = new Subject();
 
-  ngOnInit() {
-  }
+    constructor(private formBuilder: FormBuilder) {
+        this.form = formBuilder.group({
+            sender: ['', Validators.required],
+            body: ['', Validators.required],
+        });
+
+        this.formSubmitSubject
+            .filter(() => this.form.valid)
+            .map(() => this.form.value)
+            .subscribe(this.formSubmit);
+    }
 
 }
