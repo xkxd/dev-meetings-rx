e84b8236d059c97d1465defb7d317bd182a6f8dc
xkxd <krzysztof.derek@gmail.com>
2018-03-03 15:28:31 +0100

    filter links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,37 @@
 import {Component} from '@angular/core';
 import {AngularFirestore} from 'angularfire2/firestore';
-import { ViewEncapsulation } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 import {Observable} from 'rxjs/Observable';
+import {concat, concatMap, filter, map, mergeMap, switchMap, scan} from 'rxjs/operators';
+import {of} from 'rxjs/observable/of';
+import {from} from "rxjs/observable/from";
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
     encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-	title = 'app';
-	items: Observable<any[]>;
-	db: AngularFirestore;
+    title = 'app';
+    items: Observable<any[]>;
+    sitebarItems: Observable<any[]>;
+    db: AngularFirestore;
 
-	constructor(db: AngularFirestore) {
-	    this.db = db;
-		this.items = db.collection('messages', ref => ref.orderBy('timestamp', 'desc')).valueChanges();
-	}
+    constructor(db: AngularFirestore) {
+        this.db = db;
+        this.items = db.collection('messages', ref => ref.orderBy('timestamp', 'desc')).valueChanges();
+
+        this.sitebarItems = this.items.pipe(
+            switchMap(
+                (el) => from(el).pipe(
+                    filter(msg => msg.body.includes('http')),
+                    scan((acc, i) => acc.concat(i), [])
+                )
+            )
+        );
+    }
 
     add(stuff) {
         this.db.collection('messages').add({
